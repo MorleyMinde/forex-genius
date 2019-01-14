3386fd2d4843a7e5947d7abdabf157a665c2d451
Vincent P. Minde <vincentminde@gmail.com>
2019-01-15 00:04:52 +0300

    Set units to 10000

--- candles_poll.py
+++ candles_poll.py
@@ -184,9 +184,9 @@ class CandlePrinter():
         return
 
     def buy(self):
-        self.order(100000)
+        self.order(10000)
     def sell(self):
-        self.order(-100000)
+        self.order(-10000)
     def order(self,units):
         api = self.get_api()
         response = api.order.market(
